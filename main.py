6f344cb7a30e8416e6d54c449de198ad5995e988
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-05-05 18:05:36 -0400

    got song names from website using beautifulsoup.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
 
+BILLBOARD_LINK = "https://www.billboard.com/charts/hot-100/"
+USER_DATE = input("Enter the date in the format of YYY-MM-DD : ")
+link_with_date = f"{BILLBOARD_LINK}{USER_DATE}/"
+response = requests.get(url=link_with_date)
+print(response)
+soup = BeautifulSoup(response.content,"html.parser")
+song_names = soup.find('h3', class_='c-title')
+
+print(song_names.getText())
